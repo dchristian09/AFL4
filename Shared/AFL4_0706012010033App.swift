b54fc8f5ffc7b5f88f0a9a616085784c05156c2c
David Christian <dchristian09@student.ciputra.ac.id>
2022-05-24 08:42:45 +0700

    Initial Commit

---
+++ Shared/AFL4_0706012010033App.swift
@@ -0,0 +1,17 @@
+//
+//  AFL4_0706012010033App.swift
+//  Shared
+//
+//  Created by MacBook Pro on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AFL4_0706012010033App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
